4dcdbb3f801ad103db57b350d3601a52a9044c54
eriukristene <eriukristene@gmail.com>
2019-10-09 09:32:37 +0200

    Update vikenkart.js

--- scripts/vikenkart.js
+++ scripts/vikenkart.js
@@ -228,8 +228,14 @@ var polygonViken = L.polygon(
 ).addTo(mymap);
 
 // set the color for the Viken polygon
-polygonViken.setStyle({ fillColor: 'red' });
-//polygonViken.setStyle();
+polygonViken.setStyle(
+	{
+	fillColor: 'red',
+	color: 'white'
+	}
+	
+); //end setStyle()
+
 
  /*example of customizing the pop up text with links and other HTML tags /  
       var schoolOne = L.marker([59.9134, 10.8418], {icon: BigIcon});

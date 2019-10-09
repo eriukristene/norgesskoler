51dc7a17206bef58938dd8ce43701f64f5258069
eriukristene <eriukristene@gmail.com>
2019-10-09 10:44:54 +0200

    Update vikenkart.js

--- scripts/vikenkart.js
+++ scripts/vikenkart.js
@@ -1,21 +1,21 @@
 //define the icons we will use to place on the map to indicate locations
       var bigIcon = L.icon ({
        iconUrl: 'big.png',                
-       iconSize: [100,100],
+       iconSize: [50,50],
        iconAnchor: [30.072222, 31.275556],
        popupAnchor: [-3, -76]
     });
       
       var mediumIcon = L.icon ({
-       iconUrl: 'medium.png',                
-       iconSize: [75,75],
+       iconUrl: 'big.png',                
+       iconSize: [25,25],
        iconAnchor: [30.072222, 31.275556],
        popupAnchor: [-3, -76]
     });
 
       var smallIcon = L.icon ({
-       iconUrl: 'small.png',                
-       iconSize: [50,50],
+       iconUrl: 'big.png',                
+       iconSize: [12,12],
        iconAnchor: [30.072222, 31.275556],
        popupAnchor: [-3, -76]
     });

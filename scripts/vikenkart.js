66bcb03b86466ab3ae9ee2dcad23b57e2f4e09e8
eriukristene <eriukristene@gmail.com>
2019-10-10 10:32:53 +0200

    Update vikenkart.js

--- scripts/vikenkart.js
+++ scripts/vikenkart.js
@@ -1,6 +1,7 @@
 // define the map and set its location via ([latitude, longitude], map zoom)
-var mymap = L.map('mapid', {layers:[bigSchoolsGroup,mediumSchoolsGroup,smallSchoolsGroup]}).setView([59.9139, 10.7522], 9); 
-    
+//var mymap = L.map('mapid', {layers:[bigSchoolsGroup,mediumSchoolsGroup,smallSchoolsGroup]}).setView([59.9139, 10.7522], 9); 
+var mymap = L.map('mapid').setView([59.9139, 10.7522], 9);    
+
 //access the MapBox maps API, with generated API key/access token from Erin's MapBox account
     L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXJpdWtyaXN0ZW5lIiwiYSI6ImNrMWFlZDAzMDBjODQzZHBhd2kxaGJndGoifQ.qtMgz7G3f0Ptwawi3Ws_Ww', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
